e6b5d05ab11e4e308149dc9fe0639a6227bba8ae
Hama <hama75020@gmail.com>
2022-05-11 22:56:12 +0200

    create infra directory with mongoose client and make index.ts a simple entrypoint

---
+++ src/infra/mongodb/mongooseClient.ts
@@ -0,0 +1,10 @@
+import mongoose, { Connection } from 'mongoose';
+import config from '../../app/config';
+
+export default async (): Promise<Connection> => {
+    const { connection } = await mongoose.connect(config.database.url);
+
+    console.log('✌️ DB loaded and connected!');
+
+    return connection;
+};
